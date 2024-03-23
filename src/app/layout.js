5d4382a89fa5dd0a374cdeea62f992263118d70f
Rohit Bhisikar <rohitbhisikar@icloud.com>
2024-03-23 22:43:33 +0530

    favicon update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,25 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Wasted Potential Studio",
-  description: "Creative Studio",
-  icons:{
-    icon:['/favicon.ico?v=4'],
-    apple:['/apple-touch-icon.png?v=4'],
-    shortcut:['/apple-touch-icon.png']
-  }
+  description: "Creative Studio",  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="../public/images/favicon.ico" />
+      </Head>
       <body className={inter.className}>
         {children}
         <SpeedInsights/>
-        </body>
+      </body>
     </html>
   );
 }
